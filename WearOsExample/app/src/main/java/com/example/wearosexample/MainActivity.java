e5d022c0f2794617c50bff7b8f255adf184b000a
99002683 <72071846+99002683@users.noreply.github.com>
2020-12-04 17:28:58 +0530

    Added Changes in Main UI

--- WearOsExample/app/src/main/java/com/example/wearosexample/MainActivity.java
+++ WearOsExample/app/src/main/java/com/example/wearosexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
 import android.os.BatteryManager;
@@ -26,6 +27,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class MainActivity extends DigitalWatchFaceActivity {
@@ -34,13 +36,16 @@ public class MainActivity extends DigitalWatchFaceActivity {
     private static final String TIME_FORMAT_12 = "h:mm";
     private static final String DATE_FORMAT = "EEE, d MMM yyyy";
     private static final String TIME_FORMAT_AMPM = "a";
-    private static final String TIME_FORMAT_TIMEZONE = "zzz";
+    private static final String TIME_FORMAT_TIMEZONE = "Z";
     private static final String TIME_FORMAT_24_seconds = "H:mm:ss";
-    private static final String TIME_FORMAT_12_seconds = "h:mm:ss";
+    private static final String TIME_FORMAT_12_allTime = "h:mm:ss";
     //private static final String intent = Intent.ACTION_BATTERY_CHANGED;
 
+    private static final String TIME_FORMAT_12_hours = "h";
+    private static final String TIME_FORMAT_12_minutes = "mm";
+    private static final String TIME_FORMAT_12_seconds = "ss";
 
-    TextView cDate,cAMPM, cTime,cTimeZone, batteryTxt;
+    TextView cDate,cAMPM, cHours, cMinutes, cSeconds, cTimeZone, batteryTxt;
     private TimeZone tz;
     private Date date;
     private  Calendar cal;
@@ -48,37 +53,61 @@ public class MainActivity extends DigitalWatchFaceActivity {
 
     @Override
     public void onScreenDim() { //The display is dozing in a low power state; it is still on but is optimized for showing system-provided content while the device is non-interactive.
-//        cTime.setTextColor(Color.CYAN);
+//        cHours.setTextColor(Color.CYAN);
 //        batteryTxt.setTextColor(Color.CYAN);
 
-        batteryTxt.setTextColor(Color.GRAY);
-        cTime.setTextColor(Color.GRAY);
-        cDate.setTextColor(Color.GRAY);
-        cAMPM.setTextColor(Color.GRAY);
-        cTimeZone.setTextColor(Color.GRAY);
+
+        setActivityBackgroundColor(getColor(R.color.DarkGray1));
+
+        cHours.setTextColor(getResources().getColor(R.color.DarkGray));
+        cMinutes.setTextColor(getResources().getColor(R.color.DarkGray));
+        cSeconds.setTextColor(getResources().getColor(R.color.DarkGray));
+        cSeconds.setVisibility(View.INVISIBLE);
+
+        batteryTxt.setTextColor(getResources().getColor(R.color.LightSlateGray));
+        cDate.setTextColor(getResources().getColor(R.color.LightSlateGray));
+        cAMPM.setTextColor(getResources().getColor(R.color.LightSlateGray));
+        cTimeZone.setTextColor(getResources().getColor(R.color.LightSlateGray));
         cAMPM.setVisibility(View.INVISIBLE);
         cTimeZone.setVisibility(View.INVISIBLE);
+
+
+        //setActivityBackgroundColor(View.VISIBLE);
     }
 
     @Override
     public void onScreenAwake() {
         batteryTxt.setVisibility(View.VISIBLE);
-        cTime.setVisibility(View.VISIBLE);
+
+//        cHours.setTypeface(cHours.getTypeface(), Typeface.NORMAL);
+//        cMinutes.setTypeface(cMinutes.getTypeface(), Typeface.NORMAL);
+//        cSeconds.setTypeface(cSeconds.getTypeface(), Typeface.NORMAL);
+
         cDate.setVisibility(View.VISIBLE);
         cAMPM.setVisibility(View.VISIBLE);
         cTimeZone.setVisibility(View.VISIBLE);
 
+        cHours.setVisibility(View.VISIBLE);
+        cMinutes.setVisibility(View.VISIBLE);
+        cSeconds.setVisibility(View.VISIBLE);
+
+        setActivityBackgroundColor(View.INVISIBLE);
+
+
         batteryTxt.setTextColor(Color.WHITE);
-        cTime.setTextColor(Color.WHITE);
+        cHours.setTextColor(Color.WHITE);
+        cMinutes.setTextColor(Color.WHITE);
+        cSeconds.setTextColor(Color.WHITE);
         cDate.setTextColor(Color.WHITE);
         cAMPM.setTextColor(Color.WHITE);
         cTimeZone.setTextColor(Color.WHITE);
+
     }
 
     @Override
     public void onScreenOff() { //The display is off.
         batteryTxt.setVisibility(View.GONE);
-        cTime.setVisibility(View.GONE);
+        cHours.setVisibility(View.GONE);
         cDate.setVisibility(View.GONE);
         cAMPM.setVisibility(View.GONE);
         cTimeZone.setVisibility(View.GONE);
@@ -91,7 +120,11 @@ public class MainActivity extends DigitalWatchFaceActivity {
 
         cDate = (TextView)findViewById(R.id.currentDate);
         cAMPM = (TextView)findViewById(R.id.textViewAMPM);
-        cTime = (TextView) findViewById(R.id.textViewTime);
+
+        cHours = (TextView) findViewById(R.id.textViewTimeHour);
+        cMinutes = (TextView) findViewById(R.id.textViewTimeMinute);
+        cSeconds = (TextView) findViewById(R.id.textViewTimeSecond);
+
         cTimeZone = (TextView) findViewById(R.id.textViewTimeZone);
         batteryTxt = (TextView) findViewById(R.id.watchBattery);
 
@@ -116,7 +149,10 @@ public class MainActivity extends DigitalWatchFaceActivity {
     private BroadcastReceiver timeReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-           //updateTime();
+           updateTime();
+            updateHours();
+            updateMinutes();
+            updateSeconds();
         }
     };
 
@@ -139,7 +175,9 @@ public class MainActivity extends DigitalWatchFaceActivity {
 //            tz = TimeZone.getDefault();
             handler.postDelayed(this, 1000);
             updateTime();
-
+            updateHours();
+            updateMinutes();
+            updateSeconds();
         }
     };
 
@@ -157,18 +195,18 @@ public class MainActivity extends DigitalWatchFaceActivity {
 ////        View view = this.getWindow().getDecorView();
 ////        view.setBackgroundColor(#009688);
 //
-//        //cTime.getPaint().setAntiAlias(false);
+//        //cHours.getPaint().setAntiAlias(false);
 //        batteryTxt.setTextColor(Color.GRAY);
-//        cTime.setTextColor(Color.GRAY);
+//        cHours.setTextColor(Color.GRAY);
 //        cDate.setTextColor(Color.GRAY);
 //        cAMPM.setTextColor(Color.GRAY);
 //        cTimeZone.setTextColor(Color.GRAY);
 //    }
 //    @Override
 //    public void onExitAmbient(){
-//        //cTime.getPaint().setAntiAlias(true);
+//        //cHours.getPaint().setAntiAlias(true);
 //        batteryTxt.setTextColor(Color.WHITE);
-//        cTime.setTextColor(Color.WHITE);
+//        cHours.setTextColor(Color.WHITE);
 //        cDate.setTextColor(Color.WHITE);
 //        cAMPM.setTextColor(Color.WHITE);
 //        cTimeZone.setTextColor(Color.WHITE);
@@ -186,7 +224,8 @@ public class MainActivity extends DigitalWatchFaceActivity {
         SimpleDateFormat sdf, sdfAMPM, sdfCurrentDate, sdfTimeZone;
 
         Date date = new Date();
-        Calendar cal = Calendar.getInstance();
+        //Calendar cal = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"), Locale.getDefault());
         //TimeZone tz = cal.getTimeZone();
         TimeZone tz = TimeZone.getDefault();
         Log.d("Time zone","="+tz.getDisplayName());
@@ -194,16 +233,16 @@ public class MainActivity extends DigitalWatchFaceActivity {
 
 //        if(android.text.format.DateFormat.is24HourFormat(this)){
 //            sdf = new SimpleDateFormat(TIME_FORMAT_24_seconds);
-//            cTime.setVisibility(View.INVISIBLE);
+//            cHours.setVisibility(View.INVISIBLE);
 //        }
 //        else {
-//            sdf = new SimpleDateFormat(TIME_FORMAT_12_seconds);
-//            cTime.setVisibility(View.VISIBLE);
+//            sdf = new SimpleDateFormat(TIME_FORMAT_12_allTime);
+//            cHours.setVisibility(View.VISIBLE);
 //        }
 
-        sdf = new SimpleDateFormat(TIME_FORMAT_12_seconds);
-        sdf.setTimeZone(tz);
-        cTime.setText(sdf.format(date));
+//        sdf = new SimpleDateFormat(TIME_FORMAT_12_allTime);
+//        sdf.setTimeZone(tz);
+//        cHours.setText(sdf.format(date));
 
         sdfAMPM = new SimpleDateFormat(TIME_FORMAT_AMPM);
         sdfAMPM.setTimeZone(tz);
@@ -214,9 +253,14 @@ public class MainActivity extends DigitalWatchFaceActivity {
         cDate.setText(sdfCurrentDate.format(date));
 
 
-        sdfTimeZone = new SimpleDateFormat(TIME_FORMAT_TIMEZONE);
-        sdfTimeZone.setTimeZone(tz);
-        cTimeZone.setText(sdfTimeZone.format(date));
+//        sdfTimeZone = new SimpleDateFormat(TIME_FORMAT_TIMEZONE);
+//        sdfTimeZone.setTimeZone(tz);
+
+
+        Date currentLocalTime = calendar.getTime();
+        DateFormat date1 = new SimpleDateFormat(TIME_FORMAT_TIMEZONE);
+        String localTime = date1.format(currentLocalTime);
+        cTimeZone.setText(localTime);
 
 //        sdfTimeZone = new SimpleDateFormat(DATE_FORMAT);
 //        sdfTimeZone.setTimeZone(tz);
@@ -229,5 +273,45 @@ public class MainActivity extends DigitalWatchFaceActivity {
 //        //System.out.println(dateFormat.format(cal.getTime()));
 //        Log.d("Calender zone","="+dateFormat.format(cal.getTime()));
 //        cdate.setText(dateFormat.format(date));
+    }
+    private void updateHours(){
+        SimpleDateFormat sdf;
+
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        //TimeZone tz = cal.getTimeZone();
+        TimeZone tz = TimeZone.getDefault();
+
+        sdf = new SimpleDateFormat(TIME_FORMAT_12_hours);
+        sdf.setTimeZone(tz);
+        cHours.setText(sdf.format(date));
+    }
+    private void updateMinutes(){
+        SimpleDateFormat sdf;
+
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        //TimeZone tz = cal.getTimeZone();
+        TimeZone tz = TimeZone.getDefault();
+
+        sdf = new SimpleDateFormat(TIME_FORMAT_12_minutes);
+        sdf.setTimeZone(tz);
+        cMinutes.setText(sdf.format(date));
+    }
+    private void updateSeconds(){
+        SimpleDateFormat sdf;
+
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        //TimeZone tz = cal.getTimeZone();
+        TimeZone tz = TimeZone.getDefault();
+
+        sdf = new SimpleDateFormat(TIME_FORMAT_12_seconds);
+        sdf.setTimeZone(tz);
+        cSeconds.setText(sdf.format(date));
+    }
+    public void setActivityBackgroundColor(int color) {
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(color);
     }
 }
