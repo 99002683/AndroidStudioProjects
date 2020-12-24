7c9d96c12d5d704087c5c656854e6e5349f8cc04
99002683 <72071846+99002683@users.noreply.github.com>
2020-12-24 11:22:21 +0530

    Added Android Studio Codes

---
+++ AndroidGraphics/app/build/generated/source/buildConfig/debug/com/example/androidgraphics/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.androidgraphics;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.androidgraphics";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
