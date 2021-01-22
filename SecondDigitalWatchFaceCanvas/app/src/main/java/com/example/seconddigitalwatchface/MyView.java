4b2721e70362595f1734dd260c290907bab923bc
99002683 <72071846+99002683@users.noreply.github.com>
2021-01-22 18:40:38 +0530

    Added circle and round recatngle. Added day, bg and arc
    Modified Second Digital WatchFace Canvas. Added upper circle and lower round recatngle. Added day, bg and arc.

--- SecondDigitalWatchFaceCanvas/app/src/main/java/com/example/seconddigitalwatchface/MyView.java
+++ SecondDigitalWatchFaceCanvas/app/src/main/java/com/example/seconddigitalwatchface/MyView.java
@@ -1,13 +1,19 @@
 package com.example.seconddigitalwatchface;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.os.Build;
+import android.text.format.DateFormat;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.View;
 
@@ -21,6 +27,7 @@ import java.util.TimeZone;
 
 public class MyView extends View {
     Context mContext;
+    int percent;
     private int height, width = 0;
     private int padding = 0;
     private int fontSize = 0;
@@ -38,28 +45,34 @@ public class MyView extends View {
     private Paint mSecondStickPaint;
     private Paint mTickPaint;
     private Paint mCirclePaint;
+    private Paint mHandsPaint;
     private int centerX, centerY;
     private int mWidth;
     private int mHeight;
     private Calendar mCalendar;
+    private String mDescFormat;
+    private TimeZone mTimeZone;
 
     private String text = "ABC";
-    private Paint mnpaint;
+    private Paint mPaint;
     private Paint mncirclePaint;
+    RectF mRectF;
 
 
     private static boolean shouldShowSeconds = true;
     private static final String TIME_FORMAT_WITHOUT_SECONDS = "%02d:%02d";
     private static final String TIME_FORMAT_WITH_SECONDS = TIME_FORMAT_WITHOUT_SECONDS + ":%02d";
     private static final String DATE_FORMAT = "%02d-%02d-%d";  //"%02d.%02d.%d";
-    private static final String DATE_FORMAT_DAY = "%02d";
+    private static final String DATE_FORMAT_DATE = "%02d";
     private static final String DATE_FORMAT_MONTH = "MMM";
     private static final String TIME_FORMAT_24_HOUR = "HH:mm";
     private static final String TIME_FORMAT_12_HOUR = "hh:mm";
+    private static final String DATE_FORMAT_DAY = "EEE";
     private String batteryText = "100%";
 
     public MyView(Context context) {
         super(context);
+
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
@@ -69,6 +82,9 @@ public class MyView extends View {
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mContext=context;
+
+        mCalendar = Calendar.getInstance();
+        //mDescFormat = ((SimpleDateFormat) DateFormat.getTimeFormat(mContext)).toLocalizedPattern();
     }
 
     private void initClock() {
@@ -82,6 +98,8 @@ public class MyView extends View {
         hourHandTruncation = (min / 7)*3;    //5
         paint = new Paint();
         isInit = true;
+        percent = 100;
+
     }
 
     @Override
@@ -90,7 +108,7 @@ public class MyView extends View {
             initClock();
         }
 
-        canvas.drawColor(Color.BLACK);
+        canvas.drawColor(getResources().getColor(R.color.grey));
         drawCircle(canvas);
         //drawCenter(canvas);
         initializeWatchFace();
@@ -109,7 +127,7 @@ public class MyView extends View {
     }
     private void drawCircle(Canvas canvas) {
         paint.reset();
-        paint.setColor(getResources().getColor(android.R.color.white));
+        paint.setColor(getResources().getColor(android.R.color.darker_gray));
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.STROKE);
         paint.setAntiAlias(true);
@@ -178,22 +196,33 @@ public class MyView extends View {
 
         mTickPaint = new Paint();
         mTickPaint.setColor(getResources().getColor(R.color.white));
-        mTickPaint.setStrokeWidth(7);
+        mTickPaint.setStrokeWidth(getResources().getDimension(R.dimen.default_stroke_width));
         mTickPaint.setAntiAlias(true);
         mTickPaint.setStyle(Paint.Style.STROKE);
         //mTickPaint.setShadowLayer(SHADOW_RADIUS, 0, 0, mTimePaintColor);
 
         mCirclePaint = new Paint();
-        mCirclePaint.setColor(getResources().getColor(R.color.white));
-        mCirclePaint.setStrokeWidth(7);
+        mCirclePaint.setColor(getResources().getColor(R.color.black));
+        mCirclePaint.setStrokeWidth(getResources().getDimension(R.dimen.default_stroke_width));
         mCirclePaint.setAntiAlias(true);
         mCirclePaint.setStyle(Paint.Style.STROKE);
         mCirclePaint.setTextAlign(Paint.Align.CENTER);
 
+        mHandsPaint = new Paint();
+        mHandsPaint.setColor(getResources().getColor(R.color.white));
+        mHandsPaint.setStrokeWidth(getResources().getDimension(R.dimen.default_stroke_width));
+        mHandsPaint.setAntiAlias(true);
+        mHandsPaint.setStyle(Paint.Style.STROKE);
+        mHandsPaint.setTextAlign(Paint.Align.CENTER);
+
         mDayPaint = new Paint();
         mDayPaint.setColor(getResources().getColor(R.color.white));
         mDayPaint.setTextSize(getResources().getDimension(R.dimen.day_size));
-        mDayPaint.setTypeface(Typeface.SERIF);
+        //mDayPaint.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
+        mDayPaint.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mDayPaint.setLetterSpacing(0.3f);
+        }
         mDayPaint.setAntiAlias(true);
         mDayPaint.setTextAlign(Paint.Align.CENTER);
 
@@ -204,37 +233,65 @@ public class MyView extends View {
         mMonthPaint.setAntiAlias(true);
         mMonthPaint.setTextAlign(Paint.Align.CENTER);
 
-        mnpaint = new Paint();
-        mnpaint.setColor(Color.WHITE);
-        mnpaint.setTextSize(18f);
-        mnpaint.setAntiAlias(true);
-        mnpaint.setTextAlign(Paint.Align.CENTER);
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        Bitmap bmp = Bitmap.createBitmap(200, 200, conf);
 
-
+        mPaint = new Paint(Paint.FILTER_BITMAP_FLAG |
+                Paint.DITHER_FLAG |
+                Paint.ANTI_ALIAS_FLAG);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.GRAY);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(1);
+        mPaint.setColor(Color.parseColor("#33b5e5"));
+        mPaint.setStrokeWidth(15);
     }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mCalendar = Calendar.getInstance(mTimeZone != null ? mTimeZone : TimeZone.getDefault());
+        onTimeChanged();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+
     private void drawWatchFace(Canvas canvas) {
+//        DisplayMetrics displayMetrics = new DisplayMetrics();
+//
+//        ((Activity) getContext()).getWindowManager()
+//                .getDefaultDisplay()
+//                .getMetrics(displayMetrics);
+//
+//        int screenWidth = displayMetrics.widthPixels;
+//        int screenHeight = displayMetrics.heightPixels;
 
         mWidth = canvas.getWidth();
         mHeight = canvas.getHeight();
+
+//        mWidth = screenWidth;
+//        mHeight = screenHeight;
 
         float mCenterX = mWidth / 2f;
         float mCenterY = mHeight / 2f;
 
         mCalendar = Calendar.getInstance();
         Date date = new Date();
-        TimeZone tz = TimeZone.getDefault();
+        mTimeZone = TimeZone.getDefault();
 
 
         //String timeText1 = String.format(shouldShowSeconds ? TIME_FORMAT_WITH_SECONDS : TIME_FORMAT_WITHOUT_SECONDS, mCalendar.get(Calendar.HOUR),mCalendar.get(Calendar.MINUTE),mCalendar.get(Calendar.SECOND));
-        String timeText2 = String.format(TIME_FORMAT_WITHOUT_SECONDS, mCalendar.get(Calendar.HOUR),mCalendar.get(Calendar.MINUTE));
+        //String timeText2 = String.format(TIME_FORMAT_WITHOUT_SECONDS, mCalendar.get(Calendar.HOUR),mCalendar.get(Calendar.MINUTE));
+        SimpleDateFormat sdf1 = new SimpleDateFormat(TIME_FORMAT_12_HOUR);
+        sdf1.setTimeZone(mTimeZone);
+        String timeText3 = String.format(sdf1.format(date), sdf1);
 
-//        SimpleDateFormat sdf1 = new SimpleDateFormat(TIME_FORMAT_12_HOUR);
-//        sdf1.setTimeZone(tz);
-//        String timeText3 = String.format(sdf1.format(date), sdf1);
-
-        float timeXOffset = computeXOffset(timeText2, mTimePaint, mCenterX);
+        float timeXOffset = computeXOffset(timeText3, mTimePaint, mCenterX);
         float timeYOffset = mCenterY;
-        canvas.drawText(timeText2, timeXOffset, timeYOffset, mTimePaint);
+        canvas.drawText(timeText3, timeXOffset, timeYOffset, mTimePaint);
 
 //        SimpleDateFormat sdf2 = new SimpleDateFormat(DATE_FORMAT);
 //        sdf2.setTimeZone(tz);
@@ -244,28 +301,38 @@ public class MyView extends View {
         //float dateYOffset = computeYOffset(dateText, mDatePaint);
 //        canvas.drawText(dateText, dateXOffset, timeYOffset + dateYOffset, mDatePaint);
 
-        //First Circle with Analog Clock
-
-
 
 
 
         //Second Circle with Date and Month Text
-        String dateText = String.format(DATE_FORMAT_DAY, mCalendar.get(Calendar.DAY_OF_MONTH));
-        Rect bounds1 = new Rect();
-        mDayPaint.getTextBounds(dateText, 0, dateText.length(), bounds1);
+        String dateText = String.format(DATE_FORMAT_DATE, mCalendar.get(Calendar.DAY_OF_MONTH));
         canvas.drawCircle(mCenterX + timeXOffset - padding , mCenterY, (radius/4) , mCirclePaint);
-        canvas.drawText(dateText, mCenterX + timeXOffset - padding , mCenterY, mDayPaint);
+        canvas.drawText(dateText, mCenterX + timeXOffset - padding , mCenterY - (padding * 0.3f), mDatePaint);
 
         String monthText = String.format((new SimpleDateFormat(DATE_FORMAT_MONTH).format(Calendar.MONTH)));
         float dateYOffset = computeYOffset(monthText, mDatePaint);
-        Rect bounds2 = new Rect();
-        mMonthPaint.getTextBounds(monthText, 0, monthText.length(), bounds2);
         //canvas.drawCircle(mCenterX + timeXOffset - padding , mCenterY, (radius/4) , mCirclePaint);
-        canvas.drawText(monthText, mCenterX + timeXOffset - padding , mCenterY + dateYOffset, mMonthPaint);
+        canvas.drawText(monthText, mCenterX + timeXOffset - padding, mCenterY + dateYOffset - (padding * 0.3f), mMonthPaint);
+
+        //Lower Rectangle
+        mRectF = new RectF(mCenterX - timeXOffset + padding , mCenterY + (dateYOffset * 2.5f) + padding , mCenterX + timeXOffset -padding ,mCenterY +  timeXOffset );
+       // mRectF = new RectF(mCenterX -200 , mCenterY + 100, mCenterX + 200 ,mCenterY + 200);
+        canvas.drawRoundRect(mRectF, 600, 600, mCirclePaint);
+        canvas.drawText("No Upcoming Events", mCenterX , mCenterY +  timeXOffset -(padding *1.7f), mDatePaint);
 
 
+        //Upper Circle
+        String dayText = String.format(new SimpleDateFormat(DATE_FORMAT_DAY).format(Calendar.DAY_OF_MONTH));
+        canvas.drawCircle(mCenterX , mCenterY - timeXOffset + padding, radius/4, mCirclePaint);
         //canvas.drawCircle(mCenterX , mCenterY - 450, 100 + padding - 10, mCirclePaint);
+        canvas.drawText(dayText.toUpperCase(), mCenterX, mCenterY - timeXOffset + padding, mDayPaint);
+
+
+        //Upper Circle Arc
+        RectF box1 = new RectF(mCenterX - (padding *3)   ,mCenterY - timeXOffset -(padding*2f) ,mCenterX + (padding *3),mCenterY - timeXOffset + (padding*4f));
+        //RectF box = new RectF(mCenterX - (dateYOffset * 1.5f)   ,mCenterY - timeXOffset -(padding*2) ,mCenterX +(dateYOffset*1.5f),mCenterY - (dateYOffset *2.6f)-padding);
+        float sweep = 360 * percent * 0.01f;
+        canvas.drawArc(box1, 0, sweep, false, mPaint);
 
        // canvas.drawText("Circle", canvas.getWidth() / 2, canvas.getHeight() / 2, mCirclePaint);
 
@@ -302,6 +369,7 @@ public class MyView extends View {
         float mCenterX = mWidth / 2f;
         float mCenterY = mHeight / 2f;
 
+        //First Circle with Analog Clock
         String timeText2 = String.format(TIME_FORMAT_WITHOUT_SECONDS, mCalendar.get(Calendar.HOUR),mCalendar.get(Calendar.MINUTE));
         float timeXOffset = computeXOffset(timeText2, mTimePaint, mCenterX);
         float timeYOffset = mCenterY;
@@ -312,7 +380,7 @@ public class MyView extends View {
         canvas.drawLine(mCenterX - timeXOffset + padding , mCenterY,
                 (float) (mCenterX - timeXOffset+ padding + Math.cos(angle) * handRadius),
                 (float) (mCenterY + Math.sin(angle) * handRadius),
-                mCirclePaint);
+                mHandsPaint);
     }
 
     private void drawHands(Canvas canvas) {
@@ -324,4 +392,10 @@ public class MyView extends View {
         drawHand(canvas, c.get(Calendar.SECOND), false);
     }
 
+    private void onTimeChanged() {
+        mCalendar.setTimeInMillis(System.currentTimeMillis());
+        //setContentDescription(DateFormat.format(mDescFormat, mCalendar));
+        invalidate();
+    }
+    
 }
